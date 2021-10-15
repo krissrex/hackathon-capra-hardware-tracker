ccb4c13dc8b17611c37c18755bb77cd87beccae1
Kristian Rekstad <kre@capraconsulting.no>
2021-10-15 17:26:40 +0200

    Fix bug

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -42,7 +42,7 @@ async function main() {
     const product = await Product.findOne({where: {uuid}})
     const data = product.getDataValue("data")
     data.product.status = "RESERVED"
-    product.setDataValue(data, data)
+    product.setDataValue("data", data)
     await product.save()
     res.json(product)
   })
