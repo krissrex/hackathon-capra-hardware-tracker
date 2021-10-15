fed9134e1dadc109b7626cb40f97d087a57bb16b
Truls <truls.stenrud@gmail.com>
2021-10-15 14:22:42 +0200

    Make it runnable

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import "./App.css";
 import { Type, Condition, PriceCurve } from "shared-lib";
 import { Column, useTable } from "react-table";
 import ProductAggreagate from "shared-lib/dist/types/Product";
+import Table from "./components/Table/Table";
 
 function App() {
   const data = useMemo(
@@ -72,66 +73,21 @@ function App() {
     []
   );
 
-  const tableInstance = useTable({ columns, data });
-
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    tableInstance;
-
   return (
-    // apply the table props
-    <table {...getTableProps()}>
-      <thead>
+    <Table<ProductAggreagate>
+      columns={[
         {
-          // Loop over the header rows
-          headerGroups.map((headerGroup) => (
-            // Apply the header row props
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {
-                // Loop over the headers in each row
-                headerGroup.headers.map((column) => (
-                  // Apply the header cell props
-                  <th {...column.getHeaderProps()}>
-                    {
-                      // Render the header
-                      column.render("Header")
-                    }
-                  </th>
-                ))
-              }
-            </tr>
-          ))
-        }
-      </thead>
-      {/* Apply the table body props */}
-      <tbody {...getTableBodyProps()}>
+          Header: "Product",
+          accessor: "product",
+        },
+
         {
-          // Loop over the table rows
-          rows.map((row) => {
-            // Prepare the row for display
-            prepareRow(row);
-            return (
-              // Apply the row props
-              <tr {...row.getRowProps()}>
-                {
-                  // Loop over the rows cells
-                  row.cells.map((cell) => {
-                    // Apply the cell props
-                    return (
-                      <td {...cell.getCellProps()}>
-                        {
-                          // Render the cell contents
-                          cell.render("Cell")
-                        }
-                      </td>
-                    );
-                  })
-                }
-              </tr>
-            );
-          })
-        }
-      </tbody>
-    </table>
+          Header: "Product",
+          accessor: "product",
+        },
+      ]}
+      data={data}
+    />
   );
 }
 
