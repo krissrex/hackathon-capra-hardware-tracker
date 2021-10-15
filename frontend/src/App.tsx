26078692c667d7b9a9dcc523f4e585a183f6130a
Kristian Rekstad <kre@capraconsulting.no>
2021-10-15 14:50:48 +0200

    WIP: Add react-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 import "./App.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
 import ProductSearchPage from "./components/ProductSearchPage/ProductSearchPage";
 
 function App() {
