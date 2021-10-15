d14932169638a676d3b1ff3aad51c704d49e03d4
Kristian Rekstad <kre@capraconsulting.no>
2021-10-15 14:49:31 +0200

    Add hacky search

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,7 +10,9 @@ async function main() {
   await connect()
 
   app.get("/api/product", async (req, res) => {
-    const products = await Product.findAll()
+    const query = req.query // Not a good thing for production.
+
+    const products = await Product.findAll({where: {data: {...query}}})
     res.json(products)
   })
 
@@ -25,6 +27,12 @@ async function main() {
     const uuid = req.params.uuid
     await Product.destroy({where: {uuid}})
     res.json({status: "deleted"})
+  })
+
+  app.get("/api/product/:uuid", async (req, res) => {
+    const uuid = req.params.uuid
+    const product = Product.findOne({where: {uuid}})
+    res.json(product)
   })
 
   return app
